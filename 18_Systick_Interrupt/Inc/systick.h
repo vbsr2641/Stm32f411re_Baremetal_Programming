ef1da7aa279ade2589b60117794ecb2e1613df74
Veerabhadraswamy R <veerabhadraswamyr2641@gmail.com>
2026-05-16 16:57:35 +0530

    Sys-tick timer interrupt driver

---
+++ 18_Systick_Interrupt/Inc/systick.h
@@ -0,0 +1,10 @@
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+/*Function declaration of systickDelayMS*/
+void systickDelayMS(int delay);
+
+/*Function declaration of systick_1hz_interrupt*/
+void systick_1hz_interrupt(void);
+
+#endif /* SYSTICK_H_ */
